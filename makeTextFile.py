100e9815dea6a590b2316ba803663eba3babc99e
Darren <17362990052@163.com>
2018-01-27 05:26:42 -0800

    s

--- makeTextFile.py
+++ makeTextFile.py
@@ -6,19 +6,20 @@ import os
 ls = os.linesep
 
 #get filename
-
 while True:
     fname = raw_input()
     if os.path.exists(fname):
         print "ERROR: '%s' already exists" % fname
     else:
         break
+
+
 #get file content (text)  lines
 all = []
 print "\nEnter lines ('.' by itself to quit).\n"
 
-#loop until user terminates input
 
+#loop until user terminates input
 while True:
     entry = raw_input('> ')
     if entry == '.':
@@ -26,8 +27,9 @@ while True:
     else:
         all.append(entry)
 
+
 #write lines to file with proper line-ending
-fobj = open(fname, 'a')
+fobj = open(fname, 'w')
 fobj.writelines(['%s%s' % (x, ls) for x in all])
 
 fobj.close()
