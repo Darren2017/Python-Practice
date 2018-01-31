8857230310a2faffccfa448d89057f5b28256b0b
Darren <17362990052@163.com>
2018-01-31 21:38:24 +0800

    s

---
+++ test.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+def write(so):
+    fr = open(so, 'r')
+    fw = open('newone.doc', 'w')
+    for txt in fr:
+        fw.write(txt)
+    fw.close()
+def showmenu():
+    while True:    
+        choice = input('''1-combin file
+2-over
+''')
+        if choice == 1:
+            so = raw_input('please enter the name you want to combin: ' )
+            write(so)
+        if choice == 2:
+            break
+if __name__ == '__main__':
+    showmenu()
