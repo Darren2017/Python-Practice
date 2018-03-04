c3c33060c62ce46d0e50f72a9cbe711560d8120e
Darren2017 <darren@tangaimindeMacBook-Pro.local>
2018-03-05 00:04:45 +0800

    s

---
+++ login.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://account.ccnu.edu.cn/cas/login"
+
+
+ccnu_session = requests.Session()
+f = ccnu_session.get(url)
+
+cookie = f.headers.get('set-cookie')
+Cookie = cookie[0:49]
+print Cookie
+
+soup = BeautifulSoup(f.content, "html.parser")
+lt = soup.find('input', {'name' : 'lt'}) ['value']
+execution = soup.find('input',{'name' : 'execution'}) ['value']
+print 'lt : ', lt
+print 'execution : ', execution
+
+
+headers = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding':'gzip, deflate, br',
+    'Accept-Language':'zh-CN,zh;q=0.9',
+    'Cache-Control':'max-age=0',
+    'Connection':'keep-alive',
+    'Content-Length':'157',
+    'Content-Type':'application/x-www-form-urlencoded',
+    'Cookie':Cookie,
+    'Host':'account.ccnu.edu.cn',
+    'Origin':'https://account.ccnu.edu.cn',
+    'Referer':'https://account.ccnu.edu.cn/cas/login',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+}
+
+data = { 'username' : '2017211661',
+         'passward' : 'TAM137731',
+         'lt' : lt,
+         'execution' : execution,
+         '_eventId' : 'submit',
+         'submit' : '登录'
+}
+
+f = requests.post(url + Cookie, data = data, headers = headers)
+print f.headers
